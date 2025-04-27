04a04c3ff1cd6acb353d1b86be313a64bee4c418
asuka <yagi.asuka.0x06@gmail.com>
2025-04-28 03:59:20 +0900

    fix: workersからnot foundを返すときの対象をurlからpathnameに変更

--- apps/website/web/index.js
+++ apps/website/web/index.js
@@ -4,7 +4,8 @@ const STATIC_FILE = /.*\..*$/;
 
 export default {
   async fetch(request, { STATIC }) {
-    if (request.url.match(STATIC_FILE)) {
+    const url = new URL(request.url);
+    if (url.pathname.match(STATIC_FILE)) {
       return new Response("Not Found", {
         status: 404,
       });
