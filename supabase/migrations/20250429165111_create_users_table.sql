45c34322c60680d6a9f686a778e76de4e52d2c80
YumNumm <admin@yumnumm.dev>
2025-04-30 19:25:07 +0900

    chore: supabaseの依存関係を整理し、不要なパッケージを削除

---
+++ supabase/migrations/20250429165111_create_users_table.sql
@@ -0,0 +1,79 @@
+create type "public"."role" as enum ('admin', 'staff', 'sponsor', 'speaker', 'viewer', 'attendee');
+
+create table "public"."users" (
+    "id" uuid not null,
+    "role" role not null,
+    "created_at" timestamp without time zone not null default now(),
+    "updated_at" timestamp without time zone not null default now()
+);
+
+
+alter table "public"."users" enable row level security;
+
+CREATE UNIQUE INDEX users_pkey ON public.users USING btree (id);
+
+alter table "public"."users" add constraint "users_pkey" PRIMARY KEY using index "users_pkey";
+
+alter table "public"."users" add constraint "users_id_fkey" FOREIGN KEY (id) REFERENCES auth.users(id) ON DELETE CASCADE not valid;
+
+alter table "public"."users" validate constraint "users_id_fkey";
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.handle_new_user()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+ SET search_path TO 'public'
+AS $function$
+begin
+  insert into public.profiles (id)
+  values (new.id);
+  return new;
+end;
+$function$
+;
+
+grant delete on table "public"."users" to "anon";
+
+grant insert on table "public"."users" to "anon";
+
+grant references on table "public"."users" to "anon";
+
+grant select on table "public"."users" to "anon";
+
+grant trigger on table "public"."users" to "anon";
+
+grant truncate on table "public"."users" to "anon";
+
+grant update on table "public"."users" to "anon";
+
+grant delete on table "public"."users" to "authenticated";
+
+grant insert on table "public"."users" to "authenticated";
+
+grant references on table "public"."users" to "authenticated";
+
+grant select on table "public"."users" to "authenticated";
+
+grant trigger on table "public"."users" to "authenticated";
+
+grant truncate on table "public"."users" to "authenticated";
+
+grant update on table "public"."users" to "authenticated";
+
+grant delete on table "public"."users" to "service_role";
+
+grant insert on table "public"."users" to "service_role";
+
+grant references on table "public"."users" to "service_role";
+
+grant select on table "public"."users" to "service_role";
+
+grant trigger on table "public"."users" to "service_role";
+
+grant truncate on table "public"."users" to "service_role";
+
+grant update on table "public"."users" to "service_role";
+
+
