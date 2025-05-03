28a98796a5b48010a4f1563906f3076e99811029
asuka <yagi.asuka.0x06@gmail.com>
2025-05-03 18:00:33 +0900

    feat(web): サンプルとしておいていたcontentsページの削除

--- apps/website/lib/src/routes.dart
+++ apps/website/lib/src/routes.dart
@@ -1,7 +1,6 @@
 import 'package:flutterkaigi_2025_website/config.dart' as config;
 import 'package:flutterkaigi_2025_website/src/path.dart';
 import 'package:flutterkaigi_2025_website/src/routes/_404.dart' as not_found;
-import 'package:flutterkaigi_2025_website/src/routes/contents.dart' as config;
 import 'package:flutterkaigi_2025_website/src/routes/index.dart' as index;
 import 'package:flutterkaigi_2025_website/text.dart' show Language;
 import 'package:web/web.dart';
@@ -12,7 +11,6 @@ typedef Routes = Handler Function(Path path);
 
 Handler routes(Path path) => switch (path.pattern) {
   [] => index.handler,
-  ['contents', final String id] => config.handler(id),
   ['en', ...final sub] => () {
     config.user.lang = Language.en;
     return routes(Path(sub));
