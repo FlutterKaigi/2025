738b37316df436b37038aa891fa8d7c90faf5104
YumNumm <admin@yumnumm.dev>
2025-05-06 02:32:04 +0900

    chore: company_invitationに論理削除を実装

--- supabase/schemas/003_company_invitation.sql
+++ supabase/schemas/003_company_invitation.sql
@@ -3,7 +3,8 @@ CREATE TABLE public.company_invitation (
   company_id smallint REFERENCES public.companies (id) ON DELETE CASCADE,
   key text UNIQUE NOT NULL CHECK (key <> ''),
   created_at timestamp DEFAULT now() NOT NULL,
-  updated_at timestamp DEFAULT now() NOT NULL
+  updated_at timestamp DEFAULT now() NOT NULL,
+  disabled_at timestamp
 );
 
 ALTER TABLE public.company_invitation enable ROW level security;
