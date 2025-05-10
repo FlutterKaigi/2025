58e15bb9dd1cd82f1b5fefcd95201560f6eb15f6
YumNumm <admin@yumnumm.dev>
2025-05-11 04:53:27 +0900

    変更: UserApiServiceでSupabaseClientからSupabaseUtilに依存関係を変更し、ユーザー情報取得のロジックを更新

--- bff/engine/lib/service/v1/user_api_service.dart
+++ bff/engine/lib/service/v1/user_api_service.dart
@@ -1,34 +1,34 @@
-import 'dart:convert';
+import 'dart:io';
 
-import 'package:engine/provider/supabase_client.dart';
+import 'package:bff_client/bff_client.dart';
+import 'package:engine/provider/supabase_util.dart';
 import 'package:riverpod/riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
-import 'package:supabase/supabase.dart';
 
 part 'user_api_service.g.dart';
 
 @Riverpod(keepAlive: true)
 UserApiService userApiService(Ref ref) =>
-    UserApiService(supabaseClient: ref.watch(supabaseClientProvider));
+    UserApiService(supabaseUtil: ref.watch(supabaseUtilProvider));
 
 class UserApiService {
-  UserApiService({required SupabaseClient supabaseClient})
-    : _supabaseClient = supabaseClient;
+  UserApiService({required SupabaseUtil supabaseUtil})
+    : _supabaseUtil = supabaseUtil;
 
-  final SupabaseClient _supabaseClient;
+  final SupabaseUtil _supabaseUtil;
 
   @Route.get('/me')
   Future<Response> _getMe(Request request) async {
-    // 疎通確認
-    final results =
-        await _supabaseClient
-            .from('users')
-            .select()
-            .eq('id', '-1')
-            .maybeSingle();
-    return Response.ok(jsonEncode(results));
+    final result = await _supabaseUtil.extractUser(request);
+    print(result);
+    final (_, user) = result.unwrap;
+    final response = UserMeGetResponse(user: user);
+    return Response.ok(
+      response.toJson(),
+      headers: {HttpHeaders.contentTypeHeader: ContentType.json.value},
+    );
   }
 
   Router get router => _$UserApiServiceRouter(this);
