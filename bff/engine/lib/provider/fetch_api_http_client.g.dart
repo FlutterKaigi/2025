bc024a332727e09a12aa379bad7d36f75d6307f7
YumNumm <admin@yumnumm.dev>
2025-05-11 03:59:05 +0900

    add: fetchApiHttpClient Provider

---
+++ bff/engine/lib/provider/fetch_api_http_client.g.dart
@@ -0,0 +1,31 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: type=lint
+
+part of 'fetch_api_http_client.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$fetchApiHttpClientHash() =>
+    r'3f190cc0fe8ba5ecf47dc0cee9b65033482c108f';
+
+/// See also [fetchApiHttpClient].
+@ProviderFor(fetchApiHttpClient)
+final fetchApiHttpClientProvider = Provider<FetchApiHttpClient>.internal(
+  fetchApiHttpClient,
+  name: r'fetchApiHttpClientProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$fetchApiHttpClientHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef FetchApiHttpClientRef = ProviderRef<FetchApiHttpClient>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
