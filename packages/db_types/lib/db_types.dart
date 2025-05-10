3a75621c98ed4f1d9f2bae6fe4fd48d7659a497a
YumNumm <admin@yumnumm.dev>
2025-05-11 02:00:47 +0900

    refactor: db_types.dartのライブラリエクスポートを更新し、不要なファイルを削除

--- packages/db_types/lib/db_types.dart
+++ packages/db_types/lib/db_types.dart
@@ -1,8 +1,1 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library;
-
-export 'src/db_types_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/src.dart';
