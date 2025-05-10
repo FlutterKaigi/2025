a4e1b08631508a56de54e04b98cf307d6fc7af5e
YumNumm <admin@yumnumm.dev>
2025-05-11 04:53:34 +0900

    add: SupabaseUtilクラスを新規作成し、Supabaseからユーザー情報を取得する機能を実装

---
+++ bff/engine/lib/provider/supabase_util.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: type=lint
+
+part of 'supabase_util.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$supabaseUtilHash() => r'ab4b3c58f33f5db630f943c5f094cc65798fe427';
+
+/// See also [supabaseUtil].
+@ProviderFor(supabaseUtil)
+final supabaseUtilProvider = Provider<SupabaseUtil>.internal(
+  supabaseUtil,
+  name: r'supabaseUtilProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$supabaseUtilHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef SupabaseUtilRef = ProviderRef<SupabaseUtil>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
