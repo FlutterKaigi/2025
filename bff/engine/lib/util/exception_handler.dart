cc33c303bc415e92ebef5b0232cb8f46707212ab
YumNumm <admin@yumnumm.dev>
2025-05-16 20:46:33 +0900

    add: exceptionHandler関数にPostgrestExceptionとErrorを追加し、適切なHTTPレスポンスを返すように修正

--- bff/engine/lib/util/exception_handler.dart
+++ bff/engine/lib/util/exception_handler.dart
@@ -3,7 +3,9 @@ import 'dart:convert';
 import 'package:bff_client/bff_client.dart';
 import 'package:engine/provider/supabase_util.dart';
 import 'package:shelf/shelf.dart';
+import 'package:supabase/supabase.dart';
 
+/// [fn]を実行し、例外が発生した場合に、適切なHTTPレスポンスを返す関数
 Future<Response> exceptionHandler(Future<Response> Function() fn) async {
   try {
     return await fn();
@@ -19,6 +21,18 @@ Future<Response> exceptionHandler(Future<Response> Function() fn) async {
         ).toJson(),
       ),
     );
+  } on PostgrestException catch (e) {
+    return Response(
+      500,
+      body: jsonEncode(
+        ErrorResponse.errorCode(
+          code: ErrorCode.internalServerError,
+          detail:
+              'データベースとの通信中にエラーが発生しました: '
+              '[${e.code}]: ${e.message}',
+        ).toJson(),
+      ),
+    );
   } on Exception catch (e) {
     return Response(
       500,
@@ -26,6 +40,20 @@ Future<Response> exceptionHandler(Future<Response> Function() fn) async {
         ErrorResponse.errorCode(
           code: ErrorCode.internalServerError,
           detail: 'ハンドルされていない例外が発生しました: ${e.runtimeType}',
+        ).toJson(),
+      ),
+    );
+  }
+  // MEMO(@YumNumm): DartをWebAssemblyに変換した時、稀に`JavaScriptError`が発生します
+  // そのため、ここでハンドリングしています。詳しい条件等は調査中です。
+  // ignore: avoid_catching_errors
+  on Error catch (e) {
+    return Response(
+      500,
+      body: jsonEncode(
+        ErrorResponse.errorCode(
+          code: ErrorCode.internalServerError,
+          detail: 'ハンドルされていないエラーが発生しました: ${e.runtimeType}',
         ).toJson(),
       ),
     );
