577e2dc4f139e64bcca149d34c7e65cb813a028f
YumNumm <admin@yumnumm.dev>
2025-05-16 20:21:14 +0900

    remove: unused burrel file

--- packages/bff_client/lib/src/src.dart
+++ packages/bff_client/lib/src/src.dart
@@ -1,5 +1,4 @@
 export 'api/bff_api_client.dart';
 export 'api/v1/users_api_client.dart';
 export 'model/error/error_response.dart';
-export 'model/model.dart';
 export 'model/v1/users/user_me_get_response.dart';
