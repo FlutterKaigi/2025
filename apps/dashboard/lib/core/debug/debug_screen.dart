7093abb4373bc573828ee84b4f9ced0cc2c17336
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2025-06-02 00:06:01 +0900

    chore: Talker 画面へ遷移する際に動作確認用ログを出力

--- apps/dashboard/lib/core/debug/debug_screen.dart
+++ apps/dashboard/lib/core/debug/debug_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:dashboard/core/router/router.dart';
+import 'package:dashboard/core/util/talker.dart';
 import 'package:flutter/material.dart';
+import 'package:talker_flutter/talker_flutter.dart';
 
 class DebugScreen extends StatelessWidget {
   const DebugScreen({super.key});
@@ -12,7 +16,25 @@ class DebugScreen extends StatelessWidget {
       ),
       body: Center(
         child: FilledButton(
-          onPressed: () => const TalkerRoute().push<void>(context),
+          onPressed: () async {
+            // 動作確認用のログを出力
+            talker.logCustom(
+              TalkerLog(
+                'custom',
+                title: 'custom',
+              ),
+            );
+            talker.critical('critical');
+            talker.debug('debug');
+            talker.error('error');
+            talker.info('info');
+            talker.verbose('verbose');
+            talker.warning('warning');
+
+            unawaited(
+              const TalkerRoute().push<void>(context),
+            );
+          },
           child: const Text('Talker 画面へ'),
         ),
       ),
