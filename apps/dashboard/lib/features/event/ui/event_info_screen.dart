bcd7da11c79f7633bdd4a072d72a8d1d340fd947
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2025-06-29 21:52:10 +0900

    feat(dashboard): お知らせへの導線用の ListTile 作成

--- apps/dashboard/lib/features/event/ui/event_info_screen.dart
+++ apps/dashboard/lib/features/event/ui/event_info_screen.dart
@@ -14,12 +14,42 @@ class EventInfoScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: FilledButton(
-          onPressed: () async => const NewsRoute().go(context),
-          child: const Text('ニュース画面へ'),
+    return const Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            _NotificationListTile(),
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class _NotificationListTile extends StatelessWidget {
+  const _NotificationListTile({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Ink(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color(0xFFE50632),
+            Color(0xFF822FE0),
+            Color(0xFF007AFF),
+          ],
+          stops: [0.04, 0.5, 0.95],
+        ),
+      ),
+      child: ListTile(
+        leading: const Icon(Icons.notifications_outlined),
+        title: const Text('最新のお知らせ'),
+        subtitle: const Text('最新のお知らせをご確認ください'),
+        trailing: const Icon(Icons.arrow_right),
+        onTap: () async => const NewsRoute().go(context),
       ),
     );
   }
