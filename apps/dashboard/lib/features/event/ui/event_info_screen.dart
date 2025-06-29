d18165a0cd29e09af4b986da92051209a2bf4c2e
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2025-06-29 22:05:20 +0900

    feat(dashboard): イベント情報用の Card を作成

--- apps/dashboard/lib/features/event/ui/event_info_screen.dart
+++ apps/dashboard/lib/features/event/ui/event_info_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dashboard/core/assets/assets.gen.dart';
 import 'package:dashboard/core/router/router.dart';
 import 'package:flutter/material.dart';
 
@@ -19,6 +20,7 @@ class EventInfoScreen extends StatelessWidget {
         child: Column(
           children: [
             _NotificationListTile(),
+            _EventInfoCard(),
           ],
         ),
       ),
@@ -50,6 +52,49 @@ class _NotificationListTile extends StatelessWidget {
         subtitle: const Text('最新のお知らせをご確認ください'),
         trailing: const Icon(Icons.arrow_right),
         onTap: () async => const NewsRoute().go(context),
+      ),
+    );
+  }
+}
+
+class _EventInfoCard extends StatelessWidget {
+  const _EventInfoCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+
+    return Card.outlined(
+      margin: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          const SizedBox(height: 20),
+          Assets.logo.image(height: 126),
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
+              '''2025年、日本国内で Flutter をメインに扱う技術カンファレンス。Flutter や Dart の深い知見を持つ開発者によるセッションを多数企画します。''',
+              style: textTheme.bodyMedium,
+            ),
+          ),
+          const SizedBox(height: 8),
+          const ListTile(
+            leading: Icon(Icons.calendar_today),
+            title: Text('2025年11月13日(金)'),
+            subtitle: Text('10:00 ~ 19:00'),
+            dense: true,
+          ),
+          const ListTile(
+            leading: Icon(Icons.location_on),
+            title: Text('大手町プレイス ホール＆カンファレンス'),
+            subtitle: Text('東京都千代田区大手町二丁目3番1号'),
+            dense: true,
+          ),
+        ],
       ),
     );
   }
