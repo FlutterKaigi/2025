744648d3b4cf3812e44061e676feb1c853a36e29
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2025-07-01 23:14:40 +0900

    feat: お知らせの並び替えと絞り込み処理を実装

--- apps/dashboard/lib/features/news/data/news_provider.dart
+++ apps/dashboard/lib/features/news/data/news_provider.dart
@@ -6,15 +6,15 @@ part 'news_provider.g.dart';
 @riverpod
 Future<List<News>> news(Ref ref) async {
   await Future<void>.delayed(const Duration(seconds: 1));
-  return [
+  final newsList = [
     News(
       id: 0,
       text: 'FlutterKaigi mini #4 @Kyoto を開催',
       url: Uri.parse(
         'https://medium.com/flutterkaigi/flutterkaigi-mini-4-kyoto-%E3%82%92%E9%96%8B%E5%82%AC%E3%81%97%E3%81%BE%E3%81%99-85facf0ec6b2',
       ),
-      startedAt: DateTime(2025, 4, 16),
-      endedAt: DateTime(2025, 6, 30),
+      startedAt: DateTime.utc(2025, 4, 16, 9),
+      endedAt: DateTime.utc(2025, 6, 30, 9),
     ),
     News(
       id: 1,
@@ -22,7 +22,17 @@ Future<List<News>> news(Ref ref) async {
       url: Uri.parse(
         'https://medium.com/flutterkaigi/flutterkaigi-2025-%E3%82%B9%E3%83%9D%E3%83%B3%E3%82%B5%E3%83%BC%E5%8B%9F%E9%9B%86%E3%81%AB%E3%81%A4%E3%81%84%E3%81%A6-034330881a94',
       ),
-      startedAt: DateTime(2025, 4, 23),
+      startedAt: DateTime.utc(2025, 4, 23, 9),
     ),
   ];
+
+  // 開始日が新しい順にソート
+  newsList.sort((a, b) => a.startedAt.compareTo(b.startedAt));
+
+  // 終了日がない、または終了日が未来のものを返す
+  return newsList
+      .where(
+        (news) => news.endedAt == null || news.endedAt!.isAfter(DateTime.now()),
+      )
+      .toList();
 }
--- apps/dashboard/lib/features/news/data/news_provider.g.dart
+++ apps/dashboard/lib/features/news/data/news_provider.g.dart
@@ -39,7 +39,7 @@ final class NewsProvider
   }
 }
 
-String _$newsHash() => r'68c58167b2e0f37fe6a30715fe81bf5d3799c4fc';
+String _$newsHash() => r'd44028569c3e91f0f7d700dfa8fa1e3ad715bc8e';
 
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
