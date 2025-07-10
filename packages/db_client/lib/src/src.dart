1ecf60583175be906f6df040aea1fde00e63fe95
YumNumm <admin@yumnumm.dev>
2025-07-10 16:55:58 +0900

    regenerate barrel file

--- packages/db_client/lib/src/src.dart
+++ packages/db_client/lib/src/src.dart
@@ -2,6 +2,4 @@ export 'client/db_client.dart';
 export 'client/user/user_db_client.dart';
 export 'db_client_base.dart';
 export 'exception/db_exception.dart';
-export 'interop/hyperdrive.dart';
-export 'interop/pg_result.dart';
 export 'util/extract_root_json.dart';
