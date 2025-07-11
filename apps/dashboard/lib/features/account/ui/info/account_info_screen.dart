2b6fc491e66731050264a6b1815aa7acebc64496
YumNumm <admin@yumnumm.dev>
2025-07-11 20:11:12 +0900

    chore: ユーザ情報を表示する

--- apps/dashboard/lib/features/account/ui/info/account_info_screen.dart
+++ apps/dashboard/lib/features/account/ui/info/account_info_screen.dart
@@ -20,146 +20,156 @@ final class AccountInfoScreen extends ConsumerWidget {
     final state = ref.watch(authNotifierProvider);
 
     return AccountScaffold(
-      body:  Column(
-        children: [
-          Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16),
-              side: BorderSide(
-                color: Theme.of(context).colorScheme.outline,
+      body: state.when(
+        error: (error, stackTrace) => Center(
+          child: Text('Error: $error'),
+        ),
+        loading: () => const Center(
+          child: CircularProgressIndicator(),
+        ),
+        data: (user) => Column(
+          children: [
+            Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+                side: BorderSide(
+                  color: Theme.of(context).colorScheme.outline,
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    const AccountCircleImage(
+                      // TODO: ユースケースから取得した情報を表示する
+                      imageUrl:
+                          'https://storage.googleapis.com/cms-storage-bucket/0dbfcc7a59cd1cf16282.png',
+                    ),
+                    const SizedBox(height: 16),
+                    Text(
+                      // TODO: ユースケースから取得した情報を表示する
+                      'sample@google.com',
+                      style: Theme.of(context).textTheme.bodyLarge,
+                    ),
+                    const SizedBox(height: 16),
+                    Wrap(
+                      crossAxisAlignment: WrapCrossAlignment.center,
+                      spacing: 8,
+                      runSpacing: 8,
+                      children:
+                          // TODO: ユースケースから取得した情報を表示する
+                          [
+                                'Sponsor',
+                                'Google',
+                              ]
+                              .map(
+                                (text) => Chip(
+                                  label: Text(
+                                    text,
+                                    softWrap: true,
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 2,
+                                  ),
+                                ),
+                              )
+                              .toList(),
+                    ),
+                    const SizedBox(height: 16),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton.icon(
+                        onPressed: _onProfileEdit,
+                        icon: const Icon(Icons.edit),
+                        label: Text(L10n.of(context).accountProfileEdit),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Theme.of(
+                            context,
+                          ).colorScheme.secondary,
+                          foregroundColor: Theme.of(
+                            context,
+                          ).colorScheme.onSecondary,
+                          visualDensity: VisualDensity.comfortable,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    SizedBox(
+                      width: double.infinity,
+                      child: OutlinedButton.icon(
+                        onPressed: () async {
+                          await showDialog<bool?>(
+                            useRootNavigator: false,
+                            context: context,
+                            builder: (context) => AccountInvitationDialog(
+                              onCancel: () => Navigator.of(context).pop(),
+                              onFailure: () => Navigator.of(context).pop(false),
+                              onSuccess: () => Navigator.of(context).pop(true),
+                            ),
+                          ).then((value) {
+                            if (context.mounted && value != null) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content: Text(
+                                    value
+                                        ? L10n.of(
+                                            context,
+                                          ).accountInvitationCodeApplied
+                                        : L10n.of(
+                                            context,
+                                          ).accountInvitationCodeApplyFailed,
+                                  ),
+                                  behavior: SnackBarBehavior.floating,
+                                ),
+                              );
+                            }
+                          });
+                        },
+                        icon: const Icon(Icons.card_giftcard),
+                        label: Text(
+                          L10n.of(context).accountInvitationCodeInput,
+                        ),
+                        style: OutlinedButton.styleFrom(
+                          visualDensity: VisualDensity.comfortable,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-            child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                children: [
-                  const AccountCircleImage(
-                    // TODO: ユースケースから取得した情報を表示する
-                    imageUrl:
-                        'https://storage.googleapis.com/cms-storage-bucket/0dbfcc7a59cd1cf16282.png',
+            const SizedBox(height: 16),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                L10n.of(context).accountOthers,
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Expanded(
+              child: AccountOtherList(
+                items: [
+                  (
+                    title: L10n.of(context).accountCodeOfConduct,
+                    onTap: () {},
                   ),
-                  const SizedBox(height: 16),
-                  Text(
-                    // TODO: ユースケースから取得した情報を表示する
-                    'sample@google.com',
-                    style: Theme.of(context).textTheme.bodyLarge,
+                  (
+                    title: L10n.of(context).accountPrivacyPolicy,
+                    onTap: () {},
                   ),
-                  const SizedBox(height: 16),
-                  Wrap(
-                    crossAxisAlignment: WrapCrossAlignment.center,
-                    spacing: 8,
-                    runSpacing: 8,
-                    children:
-                        // TODO: ユースケースから取得した情報を表示する
-                        [
-                              'Sponsor',
-                              'Google',
-                            ]
-                            .map(
-                              (text) => Chip(
-                                label: Text(
-                                  text,
-                                  softWrap: true,
-                                  overflow: TextOverflow.ellipsis,
-                                  maxLines: 2,
-                                ),
-                              ),
-                            )
-                            .toList(),
+                  (
+                    title: L10n.of(context).accountContact,
+                    onTap: () {},
                   ),
-                  const SizedBox(height: 16),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton.icon(
-                      onPressed: _onProfileEdit,
-                      icon: const Icon(Icons.edit),
-                      label: Text(L10n.of(context).accountProfileEdit),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Theme.of(
-                          context,
-                        ).colorScheme.secondary,
-                        foregroundColor: Theme.of(
-                          context,
-                        ).colorScheme.onSecondary,
-                        visualDensity: VisualDensity.comfortable,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  SizedBox(
-                    width: double.infinity,
-                    child: OutlinedButton.icon(
-                      onPressed: () async {
-                        await showDialog<bool?>(
-                          useRootNavigator: false,
-                          context: context,
-                          builder: (context) => AccountInvitationDialog(
-                            onCancel: () => Navigator.of(context).pop(),
-                            onFailure: () => Navigator.of(context).pop(false),
-                            onSuccess: () => Navigator.of(context).pop(true),
-                          ),
-                        ).then((value) {
-                          if (context.mounted && value != null) {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                content: Text(
-                                  value
-                                      ? L10n.of(
-                                          context,
-                                        ).accountInvitationCodeApplied
-                                      : L10n.of(
-                                          context,
-                                        ).accountInvitationCodeApplyFailed,
-                                ),
-                                behavior: SnackBarBehavior.floating,
-                              ),
-                            );
-                          }
-                        });
-                      },
-                      icon: const Icon(Icons.card_giftcard),
-                      label: Text(L10n.of(context).accountInvitationCodeInput),
-                      style: OutlinedButton.styleFrom(
-                        visualDensity: VisualDensity.comfortable,
-                      ),
-                    ),
+                  (
+                    title: L10n.of(context).accountOssLicenses,
+                    onTap: () {},
                   ),
                 ],
               ),
             ),
-          ),
-          const SizedBox(height: 16),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              L10n.of(context).accountOthers,
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-          ),
-          const SizedBox(height: 8),
-          Expanded(
-            child: AccountOtherList(
-              items: [
-                (
-                  title: L10n.of(context).accountCodeOfConduct,
-                  onTap: () {},
-                ),
-                (
-                  title: L10n.of(context).accountPrivacyPolicy,
-                  onTap: () {},
-                ),
-                (
-                  title: L10n.of(context).accountContact,
-                  onTap: () {},
-                ),
-                (
-                  title: L10n.of(context).accountOssLicenses,
-                  onTap: () {},
-                ),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
