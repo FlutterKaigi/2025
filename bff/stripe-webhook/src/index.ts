f0564ebc9ab431a15502ddc7def98583630e746c
YumNumm <admin@yumnumm.dev>
2025-07-25 03:44:59 +0900

    add: bff/database package

--- bff/stripe-webhook/src/index.ts
+++ bff/stripe-webhook/src/index.ts
@@ -2,43 +2,51 @@ import { Hono } from "hono";
 import Stripe from "stripe";
 
 const app = new Hono<{
-	Bindings: Cloudflare.Env;
+  Bindings: Cloudflare.Env;
 }>();
 
 app.post("/webhook", async (c) => {
-	const stripe = new Stripe(c.env.STRIPE_API_KEY);
-	const signature = c.req.header("stripe-signature");
-	try {
-		if (!signature) {
-			return c.text("stripe-signature header is required", 400);
-		}
-		const body = await c.req.text();
-		const event = await stripe.webhooks.constructEventAsync(
-			body,
-			signature,
-			c.env.STRIPE_WEBHOOK_SECRET,
-		);
-		switch (event.type) {
-			case "payment_intent.created": {
-				console.log(event.data.object);
-				break;
-			}
-			case "payment_intent.succeeded": {
-				console.log(event.data.object);
-				break;
-			}
-			default:
-				break;
-		}
+  const stripe = new Stripe(c.env.STRIPE_API_KEY);
+  const signature = c.req.header("stripe-signature");
+  try {
+    if (!signature) {
+      return c.text("stripe-signature header is required", 400);
+    }
+    const body = await c.req.text();
+    const event = await stripe.webhooks.constructEventAsync(
+      body,
+      signature,
+      c.env.STRIPE_WEBHOOK_SECRET
+    );
+    switch (event.type) {
+      case "payment_intent.created": {
+        console.log(event.data.object);
+        break;
+      }
+      case "payment_intent.succeeded": {
+        console.log(event.data.object);
+        break;
+      }
+      case "payment_intent.canceled": {
+        console.log(event.data.object);
+        break;
+      }
+      case "payment_intent.payment_failed": {
+        console.log(event.data.object);
+        break;
+      }
+      default:
+        break;
+    }
 
-		return c.text("", 200);
-	} catch (err) {
-		const errorMessage = `⚠️  Webhook signature verification failed. ${
-			err instanceof Error ? err.message : "Internal server error"
-		}`;
-		console.log(errorMessage);
-		return c.text(errorMessage, 400);
-	}
+    return c.text("", 200);
+  } catch (err) {
+    const errorMessage = `⚠️  Webhook signature verification failed. ${
+      err instanceof Error ? err.message : "Internal server error"
+    }`;
+    console.log(errorMessage);
+    return c.text(errorMessage, 400);
+  }
 });
 
 export default app;
