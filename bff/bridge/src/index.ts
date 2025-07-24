6780e6fc25e2a73f8b4aa2d4a389709562fe4ab5
YumNumm <admin@yumnumm.dev>
2025-07-25 03:23:49 +0900

    format

--- bff/bridge/src/index.ts
+++ bff/bridge/src/index.ts
@@ -1,30 +1,31 @@
 import { Container, getRandom } from "@cloudflare/containers";
 
 export class BffEngine extends Container<
-  Env & {
-    SUPABASE_URL: string;
-    SUPABASE_SERVICE_ROLE_KEY: string;
-    POSTGRES_URL: string;
-  }
+	Env & {
+		SUPABASE_URL: string;
+
+		SUPABASE_SERVICE_ROLE_KEY: string;
+		POSTGRES_URL: string;
+	}
 > {
-  defaultPort = 8080;
-  sleepAfter = "30s";
-  envVars = {
-    SUPABASE_URL: this.env.SUPABASE_URL,
-    SUPABASE_SERVICE_ROLE_KEY: this.env.SUPABASE_SERVICE_ROLE_KEY,
-    CF_VERSION_METADATA_ID: this.env.CF_VERSION_METADATA.id,
-    CF_VERSION_METADATA_TAG: this.env.CF_VERSION_METADATA.tag,
-    CF_VERSION_METADATA_TIMESTAMP: this.env.CF_VERSION_METADATA.timestamp,
-    POSTGRES_URL: this.env.POSTGRES_URL,
-  };
+	defaultPort = 8080;
+	sleepAfter = "30s";
+	envVars = {
+		SUPABASE_URL: this.env.SUPABASE_URL,
+		SUPABASE_SERVICE_ROLE_KEY: this.env.SUPABASE_SERVICE_ROLE_KEY,
+		CF_VERSION_METADATA_ID: this.env.CF_VERSION_METADATA.id,
+		CF_VERSION_METADATA_TAG: this.env.CF_VERSION_METADATA.tag,
+		CF_VERSION_METADATA_TIMESTAMP: this.env.CF_VERSION_METADATA.timestamp,
+		POSTGRES_URL: this.env.POSTGRES_URL,
+	};
 }
 
 const INSTANCE_COUNT = 1;
 
 export default {
-  fetch: async (request: Request, env: Env) => {
-    const containerInstance = await getRandom(env.bff_engine, INSTANCE_COUNT);
-    const response = await containerInstance.fetch(request);
-    return response;
-  },
+	fetch: async (request: Request, env: Env) => {
+		const containerInstance = await getRandom(env.bff_engine, INSTANCE_COUNT);
+		const response = await containerInstance.fetch(request);
+		return response;
+	},
 };
--- bff/stripe-webhook/drizzle.config.ts
+++ bff/stripe-webhook/drizzle.config.ts
@@ -2,13 +2,13 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  dialect: "postgresql",
-  out: "./drizzle",
-  dbCredentials: {
-    url:
-      process.env.POSTGRES_URL ??
-      (() => {
-        throw new Error("POSTGRES_URL is not set");
-      })(),
-  },
+	dialect: "postgresql",
+	out: "./drizzle",
+	dbCredentials: {
+		url:
+			process.env.POSTGRES_URL ??
+			(() => {
+				throw new Error("POSTGRES_URL is not set");
+			})(),
+	},
 });
