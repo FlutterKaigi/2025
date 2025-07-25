d9e80c6662a207c5037ef4ad9ef5fe7cd7496d4d
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2025-07-25 19:30:38 +0900

    企業スポンサーを追加

--- supabase/schemas/003_sponsors.sql
+++ supabase/schemas/003_sponsors.sql
@@ -1,3 +1,4 @@
+-- 個人スポンサー
 CREATE TABLE public.sponsor_individuals (
   id smallint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   individual_id smallint REFERENCES public.individuals (id) ON DELETE CASCADE,
@@ -6,6 +7,16 @@ CREATE TABLE public.sponsor_individuals (
 
 ALTER TABLE public.sponsor_individuals enable ROW level security;
 
+-- 企業スポンサー
+CREATE TABLE public.sponsor_companies (
+  id smallint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  company_id smallint REFERENCES public.companies (id) ON DELETE CASCADE,
+  created_at timestamp DEFAULT now() NOT NULL
+);
+
+ALTER TABLE public.sponsor_companies enable ROW level security;
+
+-- ネーミングライツスポンサー
 CREATE TABLE public.sponsor_naming_rights (
   id smallint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   company_id smallint REFERENCES public.companies (id) ON DELETE CASCADE,
@@ -14,6 +25,7 @@ CREATE TABLE public.sponsor_naming_rights (
 
 ALTER TABLE public.sponsor_naming_rights enable ROW level security;
 
+-- ネームプレートスポンサー
 CREATE TABLE public.sponsor_nameplate (
   id smallint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   company_id smallint REFERENCES public.companies (id) ON DELETE CASCADE,
@@ -22,6 +34,7 @@ CREATE TABLE public.sponsor_nameplate (
 
 ALTER TABLE public.sponsor_nameplate enable ROW level security;
 
+-- ランチスポンサー
 CREATE TABLE public.sponsor_lunch (
   id smallint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   company_id smallint REFERENCES public.companies (id) ON DELETE CASCADE,
@@ -30,6 +43,7 @@ CREATE TABLE public.sponsor_lunch (
 
 ALTER TABLE public.sponsor_lunch enable ROW level security;
 
+-- スカラーシップスポンサー
 CREATE TABLE public.sponsor_scholarship (
   id smallint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   company_id smallint REFERENCES public.companies (id) ON DELETE CASCADE,
