8eb60a75673f373d728d7d9ea660552049a5033b
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2025-07-26 16:09:27 +0900

    feat: 新しいお知らせデータを作成

---
+++ supabase/seeds/005_news.sql
@@ -0,0 +1,15 @@
+INSERT INTO
+    public.news (title, url, starts_at, ends_at)
+VALUES
+    (
+        'FlutterKaigi mini #4 @Kyoto を開催',
+        'https://medium.com/flutterkaigi/flutterkaigi-mini-4-kyoto-%E3%82%92%E9%96%8B%E5%82%AC%E3%81%97%E3%81%BE%E3%81%99-85facf0ec6b2',
+        '2025-04-16 09:00:00+00'::timestamp,
+        '2025-06-30 09:00:00+00'::timestamp
+    ),
+    (
+        'FlutterKaigi 2025 スポンサー募集を開始',
+        'https://medium.com/flutterkaigi/flutterkaigi-2025-%E3%82%B9%E3%83%9D%E3%83%B3%E3%82%B5%E3%83%BC%E5%8B%9F%E9%9B%86%E3%81%AB%E3%81%A4%E3%81%84%E3%81%A6-034330881a94',
+        '2025-04-23 09:00:00+00'::timestamp,
+        NULL
+    );
