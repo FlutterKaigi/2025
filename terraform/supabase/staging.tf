9d9b11e782e20a2c1b213b68f6d662718f08642a
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2025-07-27 19:51:36 +0900

    feat: 匿名サインインを有効化

--- terraform/supabase/staging.tf
+++ terraform/supabase/staging.tf
@@ -40,7 +40,7 @@ resource "supabase_settings" "staging" {
   # https://api.supabase.com/api/v1#tag/auth/patch/v1/projects/{ref}/config/auth
   auth = jsonencode({
     external_email_enabled                = false
-    external_anonymous_user_enabled       = false
+    external_anonymous_users_enabled      = true
     external_google_enabled               = true
     external_google_additional_client_ids = null
     external_google_client_id             = var.SUPABASE_GOOGLE_AUTH_CLIENT_ID
