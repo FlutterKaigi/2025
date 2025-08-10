5683dcd0a67e5cb9ced0c349436419003c7ca511
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2025-08-10 23:59:06 +0900

    refactor: sponsor_db_clientを改善 - Sql.namedを使用してクエリを安全化 - UndecodedBytes対応を追加 - インデックスベースアクセスから名前付きアクセスに変更 - 不要なtry-catchとログ出力を削除 - コードの可読性と保守性を向上

--- packages/db_client/lib/src/client/sponsor/sponsor_db_client.dart
+++ packages/db_client/lib/src/client/sponsor/sponsor_db_client.dart
@@ -9,70 +9,47 @@ class SponsorDbClient {
 
   /// 企業スポンサーの詳細情報を取得
   Future<List<CompanySponsorDetail>> getCompanySponsors() async {
-    const query = '''
-      SELECT 
-        c.id,
-        c.name,
-        c.logo_name,
-        sc.sponsor_type,
-        bsc.basic_plan_type,
-        ARRAY_AGG(sco.option_plan_type) FILTER (WHERE sco.option_plan_type IS NOT NULL) as option_plan_types,
-        sc.created_at
-      FROM companies c
-      INNER JOIN sponsor_companies sc ON c.id = sc.company_id
-      LEFT JOIN basic_sponsor_companies bsc ON sc.id = bsc.sponsor_company_id
-      LEFT JOIN sponsor_company_options sco ON sc.id = sco.sponsor_company_id
-      GROUP BY c.id, c.name, c.logo_name, sc.sponsor_type, bsc.basic_plan_type, sc.created_at
-      ORDER BY 
-        CASE bsc.basic_plan_type
-          WHEN 'platinum' THEN 1
-          WHEN 'gold' THEN 2
-          WHEN 'silver' THEN 3
-          WHEN 'bronze' THEN 4
-          ELSE 5
-        END,
-        c.name
-    ''';
+    final result = await _connection.execute(
+      Sql.named('''
+        SELECT 
+          c.id,
+          c.name,
+          c.logo_name,
+          sc.sponsor_type,
+          bsc.basic_plan_type,
+          sc.created_at
+        FROM companies c
+        INNER JOIN sponsor_companies sc ON c.id = sc.company_id
+        LEFT JOIN basic_sponsor_companies bsc ON sc.id = bsc.sponsor_company_id
+        ORDER BY 
+          CASE bsc.basic_plan_type
+            WHEN 'platinum' THEN 1
+            WHEN 'gold' THEN 2
+            WHEN 'silver' THEN 3
+            WHEN 'bronze' THEN 4
+            ELSE 5
+          END,
+          c.name
+      '''),
+    );
 
-    final results = await _connection.execute(query);
     final sponsors = <CompanySponsorDetail>[];
+    for (final row in result) {
+      final rowMap = row.toColumnMap();
 
-    for (final row in results) {
-      final optionPlanTypes = <OptionPlanType>[];
-      if (row[5] != null) {
-        final optionTypes = row[5]! as List<dynamic>;
-        for (final optionType in optionTypes) {
-          switch (optionType as String) {
-            case 'naming_rights_hall':
-              optionPlanTypes.add(OptionPlanType.namingRightsHall);
-            case 'naming_rights_room':
-              optionPlanTypes.add(OptionPlanType.namingRightsRoom);
-            case 'nameplate':
-              optionPlanTypes.add(OptionPlanType.nameplate);
-            case 'lunch':
-              optionPlanTypes.add(OptionPlanType.lunch);
-            case 'scholarship':
-              optionPlanTypes.add(OptionPlanType.scholarship);
-          }
-        }
-      }
-
-      BasicPlanType? basicPlanType;
-      if (row[4] != null) {
-        switch (row[4]! as String) {
-          case 'platinum':
-            basicPlanType = BasicPlanType.platinum;
-          case 'gold':
-            basicPlanType = BasicPlanType.gold;
-          case 'silver':
-            basicPlanType = BasicPlanType.silver;
-          case 'bronze':
-            basicPlanType = BasicPlanType.bronze;
-        }
+      // PostgreSQL enum型の値を適切に処理
+      final sponsorTypeValue = rowMap['sponsor_type'];
+      String sponsorTypeStr;
+      if (sponsorTypeValue is String) {
+        sponsorTypeStr = sponsorTypeValue;
+      } else if (sponsorTypeValue is UndecodedBytes) {
+        sponsorTypeStr = sponsorTypeValue.asString;
+      } else {
+        sponsorTypeStr = sponsorTypeValue.toString();
       }
 
       CompanySponsorType sponsorType;
-      switch (row[3]! as String) {
+      switch (sponsorTypeStr) {
         case 'basic':
           sponsorType = CompanySponsorType.basic;
         case 'community':
@@ -85,15 +62,40 @@ class SponsorDbClient {
           sponsorType = CompanySponsorType.none;
       }
 
+      BasicPlanType? basicPlanType;
+      final basicPlanTypeValue = rowMap['basic_plan_type'];
+      if (basicPlanTypeValue != null) {
+        String basicPlanTypeStr;
+        if (basicPlanTypeValue is String) {
+          basicPlanTypeStr = basicPlanTypeValue;
+        } else if (basicPlanTypeValue is UndecodedBytes) {
+          basicPlanTypeStr = basicPlanTypeValue.asString;
+        } else {
+          basicPlanTypeStr = basicPlanTypeValue.toString();
+        }
+
+        switch (basicPlanTypeStr) {
+          case 'platinum':
+            basicPlanType = BasicPlanType.platinum;
+          case 'gold':
+            basicPlanType = BasicPlanType.gold;
+          case 'silver':
+            basicPlanType = BasicPlanType.silver;
+          case 'bronze':
+            basicPlanType = BasicPlanType.bronze;
+          default:
+            basicPlanType = BasicPlanType.bronze;
+        }
+      }
+
       sponsors.add(
         CompanySponsorDetail(
-          id: row[0]! as int,
-          name: row[1]! as String,
-          logoName: row[2]! as String,
+          id: rowMap['id'] as int,
+          name: rowMap['name'] as String,
+          logoName: rowMap['logo_name'] as String,
           sponsorType: sponsorType,
           basicPlanType: basicPlanType,
-          optionPlanTypes: optionPlanTypes.isNotEmpty ? optionPlanTypes : null,
-          createdAt: row[6]! as DateTime,
+          createdAt: rowMap['created_at'] as DateTime,
         ),
       );
     }
@@ -103,26 +105,28 @@ class SponsorDbClient {
 
   /// 個人スポンサーの詳細情報を取得
   Future<List<IndividualSponsorDetail>> getIndividualSponsors() async {
-    const query = '''
-      SELECT 
-        i.id,
-        u.raw_user_meta_data->>'name' as user_name,
-        si.created_at
-      FROM individuals i
-      INNER JOIN sponsor_individuals si ON i.id = si.individual_id
-      INNER JOIN users u ON i.user_id = u.id
-      ORDER BY si.created_at DESC
-    ''';
+    final result = await _connection.execute(
+      Sql.named('''
+        SELECT 
+          i.id,
+          au.raw_user_meta_data->>'name' as user_name,
+          si.created_at
+        FROM individuals i
+        INNER JOIN sponsor_individuals si ON i.id = si.individual_id
+        INNER JOIN auth.users au ON i.user_id = au.id
+        ORDER BY si.created_at DESC
+      '''),
+    );
 
-    final results = await _connection.execute(query);
     final sponsors = <IndividualSponsorDetail>[];
+    for (final row in result) {
+      final rowMap = row.toColumnMap();
 
-    for (final row in results) {
       sponsors.add(
         IndividualSponsorDetail(
-          id: row[0]! as int,
-          userName: row[1] as String?,
-          createdAt: row[2]! as DateTime,
+          id: rowMap['id'] as int,
+          userName: rowMap['user_name'] as String?,
+          createdAt: rowMap['created_at'] as DateTime,
         ),
       );
     }
