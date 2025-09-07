a4b6919d5f89e2bedea57753854b542e224106c7
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2025-09-08 03:48:29 +0900

    fix: 不要なものを削除

--- apps/app/lib/features/ticket/ui/ticket_screen.dart
+++ apps/app/lib/features/ticket/ui/ticket_screen.dart
@@ -1,7 +1,6 @@
 import 'package:app/core/designsystem/components/error_view.dart';
 import 'package:app/features/ticket/data/notifier/ticket_checkout_notifier.dart';
 import 'package:app/features/ticket/data/provider/ticket_items_provider.dart';
-import 'package:app/features/ticket/ui/components/active_checkout_screen.dart';
 import 'package:app/features/ticket/ui/components/ticket_list_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -38,10 +37,6 @@ class TicketScreen extends ConsumerWidget {
           ),
         ),
         data: (checkout) {
-          // 進行中のチェックアウトセッションがある場合
-          if (checkout.isNotEmpty) {
-            return ActiveCheckoutScreen(checkoutId: checkout.first.checkout.id);
-          }
           return const TicketListScreen();
         },
       ),
