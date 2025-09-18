904a143e8cb708db2aaff25005089ff729a468ef
YumNumm <admin@yumnumm.dev>
2025-09-18 21:20:11 +0900

    index.tsのリファクタリングを行い、CORSミドルウェアの処理を整理しました。また、エラーハンドリングやプロキシ機能の処理を改善し、コードの整形を行いました。

--- bff/bridge/src/index.ts
+++ bff/bridge/src/index.ts
@@ -7,63 +7,63 @@ import { secureHeaders } from "hono/secure-headers";
 import { timing } from "hono/timing";
 
 const env = workersEnv as Cloudflare.Env & {
-  ENVIRONMENT: "development" | "staging" | "production";
+	ENVIRONMENT: "development" | "staging" | "production";
 };
 
 const app = new Hono()
-  .onError((err, c) => {
-    console.error(err);
-    return c.json({ message: "Internal Server Error", error: err }, 500);
-  })
-  .use(
-    timing({
-      autoEnd: true,
-    })
-  )
-  // CORS
-  .use("*", async (c, next) => {
-    const corsMiddlewareHandler = cors({
-      origin: (origin) => {
-        if (!origin) return null;
+	.onError((err, c) => {
+		console.error(err);
+		return c.json({ message: "Internal Server Error", error: err }, 500);
+	})
+	.use(
+		timing({
+			autoEnd: true,
+		}),
+	)
+	// CORS
+	.use("*", async (c, next) => {
+		const corsMiddlewareHandler = cors({
+			origin: (origin) => {
+				if (!origin) return null;
 
-        switch (env.ENVIRONMENT) {
-          case "development": {
-            return "*";
-          }
-          case "staging": {
-            return origin.endsWith("flutter-kaigi.workers.dev")
-              ? origin
-              : "http://localhost:8080";
-          }
-          case "production": {
-            return "https://2025-app.flutterkaigi.jp";
-          }
-        }
-      },
-    });
-    return corsMiddlewareHandler(c, next);
-  })
-  // CSRF
-  .use("*", csrf())
-  // Secure Headers
-  .use("*", secureHeaders())
-  .all("*", async (c) => {
-    const originalUrl = new URL(c.req.url);
-    const host = env.BFF_API_HOST;
-    const targetUrl = new URL(originalUrl.toString());
-    targetUrl.host = host;
-    targetUrl.search = originalUrl.search;
-    console.log("targetUrl", targetUrl.toString());
+				switch (env.ENVIRONMENT) {
+					case "development": {
+						return "*";
+					}
+					case "staging": {
+						return origin.endsWith("flutter-kaigi.workers.dev")
+							? origin
+							: "http://localhost:8080";
+					}
+					case "production": {
+						return "https://2025-app.flutterkaigi.jp";
+					}
+				}
+			},
+		});
+		return corsMiddlewareHandler(c, next);
+	})
+	// CSRF
+	.use("*", csrf())
+	// Secure Headers
+	.use("*", secureHeaders())
+	.all("*", async (c) => {
+		const originalUrl = new URL(c.req.url);
+		const host = env.BFF_API_HOST;
+		const targetUrl = new URL(originalUrl.toString());
+		targetUrl.host = host;
+		targetUrl.search = originalUrl.search;
+		console.log("targetUrl", targetUrl.toString());
 
-    const response = await proxy(targetUrl.toString(), {
-      ...c.req,
-      headers: {
-        ...c.req.header(),
-        "X-Forwarded-Host": c.req.header("host"),
-      },
-    });
-    return response;
-  });
+		const response = await proxy(targetUrl.toString(), {
+			...c.req,
+			headers: {
+				...c.req.header(),
+				"X-Forwarded-Host": c.req.header("host"),
+			},
+		});
+		return response;
+	});
 // .all("*", async (c) => {
 // 	startTime(c, "request");
 // 	startTime(c, "wait_for_instance_start");
@@ -78,5 +78,5 @@ const app = new Hono()
 // });
 
 export default {
-  fetch: app.fetch,
+	fetch: app.fetch,
 };
