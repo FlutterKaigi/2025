303f309b3740ac62d58c9d3d2bc9015a0215501a
YumNumm <admin@yumnumm.dev>
2025-09-22 14:55:35 +0900

    add: sakuracloud related services

---
+++ terraform/sakuracloud/modules/docker/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    docker = {
+      # https://registry.terraform.io/providers/kreuzwerker/docker/latest
+      source = "kreuzwerker/docker"
+      version = "3.6.2"
+    }
+  }
+}
+
+provider "docker" {
+  registry_auth {
+    address  = var.docker_registry_address
+    username = var.docker_registry_username
+    password = var.docker_registry_password
+  }
+}
