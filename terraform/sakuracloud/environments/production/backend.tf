10144a9ce664661070295bb14c2b08061e9785b0
YumNumm <admin@yumnumm.dev>
2025-10-04 13:38:51 +0900

    fix: files

---
+++ terraform/sakuracloud/environments/production/backend.tf
@@ -0,0 +1,14 @@
+terraform {
+  backend "s3" {
+    bucket                      = "tf-state"
+    key                         = "flutterkaigi-2025-sakuracloud-production.tfstate"
+    region                      = "auto"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
+    endpoints                   = { s3 = "https://cdd8f59359fe226645e7b541cdc53b57.r2.cloudflarestorage.com" }
+  }
+}
