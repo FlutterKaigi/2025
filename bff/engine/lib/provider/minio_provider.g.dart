0dbacad712d355fcd3108823ab1a2f555aa759fe
YumNumm <admin@yumnumm.dev>
2025-10-18 20:51:33 +0900

    [Infra] ProfileShareで署名付きURLを取得する

---
+++ bff/engine/lib/provider/minio_provider.g.dart
@@ -0,0 +1,52 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: type=lint, duplicate_ignore
+
+part of 'minio_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(minio)
+const minioProvider = MinioProvider._();
+
+final class MinioProvider extends $FunctionalProvider<Minio, Minio, Minio>
+    with $Provider<Minio> {
+  const MinioProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'minioProvider',
+        isAutoDispose: false,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$minioHash();
+
+  @$internal
+  @override
+  $ProviderElement<Minio> $createElement($ProviderPointer pointer) =>
+      $ProviderElement(pointer);
+
+  @override
+  Minio create(Ref ref) {
+    return minio(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(Minio value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<Minio>(value),
+    );
+  }
+}
+
+String _$minioHash() => r'0013408d2a95ac18bd6e8c71e5517870b4d6c3ac';
