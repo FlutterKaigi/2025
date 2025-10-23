3c5eaa35e573aff2416e2bb842dca7a626c20aa2
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2025-10-23 23:32:59 +0900

    feat: スタッフメンバー一覧を取得するAPIを実装

---
+++ bff/engine/lib/routes/staff_member_api_service.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: type=lint, duplicate_ignore
+
+part of 'staff_member_api_service.dart';
+
+// **************************************************************************
+// ShelfRouterGenerator
+// **************************************************************************
+
+Router _$StaffMemberApiServiceRouter(StaffMemberApiService service) {
+  final router = Router();
+  router.add('GET', r'/staff-members', service._getStaffMembers);
+  return router;
+}
