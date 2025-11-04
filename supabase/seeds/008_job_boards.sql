71b8d2b6f0c03a5ee52571b1ddafd054f0030ffe
Daichi Aoki <fremie.off@gmail.com>
2025-11-04 18:02:53 +0900

    refactor(): job_boards の seed の image URL を修正

--- supabase/seeds/008_job_boards.sql
+++ supabase/seeds/008_job_boards.sql
@@ -1,15 +1,15 @@
 -- Insert job boards data
 INSERT INTO job_boards (id, url, alt_text, image_name) VALUES
-    (1, 'https://job.persona-ats.com/ja/assign/jobs?filters=zmnNB_H2Q52r7D9fUwEQ6A', '株式会社アサインのジョブボード', 'https://drive.google.com/open?id=1IN3_AMQ1RMrfn7osq-yHXUqgsCp8_Jqg'),
-    (2, 'https://hrmos.co/pages/cyberagent-group', 'サイバーエージェント', 'https://drive.google.com/open?id=1BUvDVxKn418mOLgCjjuLxR5_Y5uER5cZ'),
-    (3, 'https://magicpod.com/corporate/recruit/', '株式会社MagicPod', 'https://drive.google.com/open?id=13TF0XTLBwvhLZXWmmLSNJvMHfQKK5upl'),
-    (4, 'https://genda.jp/careers/', '株式会社GENDA 採用情報', 'https://drive.google.com/open?id=19-QhUppgcSSaekxbFwniPp2x85JKbWWy'),
-    (5, 'https://jobs.m3.com/engineer/', 'M3 エンジニアリンググループ : We''re App Developers!', 'https://drive.google.com/open?id=1eTDcanHQnEPn_we1q6zxKztb3J74hboo'),
-    (6, 'https://www.team-lab.com/recruit/career/flutter/', 'チームラボ Flutterエンジニア積極採用中！', 'https://drive.google.com/open?id=17aA7jxSg3AyMek-Dp9k_uWtLpiEn6Oxu'),
-    (7, 'https://recruit.soda-inc.jp/', '株式会社SODA', 'https://drive.google.com/file/d/1hIvSu0uSFPQXJTb5BvgXRVMSLxGWWdNH/view?usp=drive_link'),
-    (8, 'https://www.hacomono.co.jp/recruit/engineer/', '株式会社hacomono', 'https://drive.google.com/open?id=1lmxbVkZUIAkoa72aY7KTm6xow9p4j48f'),
-    (9, 'https://corporate.iiba.space/career', E'株式会社iibaの採用ページ\nhttps://corporate.iiba.space/career', 'https://drive.google.com/open?id=1Hm7ITKIT00k-jeSMuaS6XDQ_-3F_YraN'),
-    (10, 'https://hrmos.co/pages/simplex/jobs/60000001', 'シンプレクス株式会社', 'https://drive.google.com/file/d/1HJOVQs_ME-pRnDdm0X_-kUz_O6-3_wDs/view?usp=drive_link'),
-    (11, 'https://hrmos.co/pages/zozo/jobs/1809846973241688329', 'ZOZO, Inc. / ZOZOマッチ', 'https://drive.google.com/open?id=1_ToJt3RwQWr7eVienCBatUvrygXvC1Rg'),
-    (12, 'https://recruit.linc-well.com/engineer', '株式会社Linc''well 最高の医療体験を作りたい Flutter エンジニア募集', 'https://drive.google.com/file/d/1TZRTjQuIBgdlGfKwR2aEfpZVoDMabK4E/view?usp=drive_link'),
-    (13, 'https://youtrust.jp/companies/youtrust', ' ', 'https://drive.google.com/file/d/19kgT-zC0Xgr6kRKxB2tR9o9NnamUw_Oq/view?usp=drive_link');
+    (1, 'https://job.persona-ats.com/ja/assign/jobs?filters=zmnNB_H2Q52r7D9fUwEQ6A', '株式会社アサインのジョブボード', 'https://2025-bucket.flutterkaigi.jp/job_board/assign.png'),
+    (2, 'https://hrmos.co/pages/cyberagent-group', 'サイバーエージェント', 'https://2025-bucket.flutterkaigi.jp/job_board/cyberagent.png'),
+    (3, 'https://magicpod.com/corporate/recruit/', '株式会社MagicPod', 'https://2025-bucket.flutterkaigi.jp/job_board/magicpod.png'),
+    (4, 'https://genda.jp/careers/', '株式会社GENDA 採用情報', 'https://2025-bucket.flutterkaigi.jp/job_board/genda.png'),
+    (5, 'https://jobs.m3.com/engineer/', 'M3 エンジニアリンググループ : We''re App Developers!', 'https://2025-bucket.flutterkaigi.jp/job_board/m3.png'),
+    (6, 'https://www.team-lab.com/recruit/career/flutter/', 'チームラボ Flutterエンジニア積極採用中！', 'https://2025-bucket.flutterkaigi.jp/job_board/teamlab.png'),
+    (7, 'https://recruit.soda-inc.jp/', '株式会社SODA', 'https://2025-bucket.flutterkaigi.jp/job_board/soda.png'),
+    (8, 'https://www.hacomono.co.jp/recruit/engineer/', '株式会社hacomono', 'https://2025-bucket.flutterkaigi.jp/job_board/hacomono.png'),
+    (9, 'https://corporate.iiba.space/career', E'株式会社iibaの採用ページ\nhttps://corporate.iiba.space/career', 'https://2025-bucket.flutterkaigi.jp/job_board/hacomono.png'),
+    (10, 'https://hrmos.co/pages/simplex/jobs/60000001', 'シンプレクス株式会社', 'https://2025-bucket.flutterkaigi.jp/job_board/simplex.png'),
+    (11, 'https://hrmos.co/pages/zozo/jobs/1809846973241688329', 'ZOZO, Inc. / ZOZOマッチ', 'https://2025-bucket.flutterkaigi.jp/job_board/zozo.png'),
+    (12, 'https://recruit.linc-well.com/engineer', '株式会社Linc''well 最高の医療体験を作りたい Flutter エンジニア募集', 'https://2025-bucket.flutterkaigi.jp/job_board/linkwell.png'),
+    (13, 'https://youtrust.jp/companies/youtrust', ' ', 'https://2025-bucket.flutterkaigi.jp/job_board/youtrust.png');
