a9d510282c65481362275956d2d5c8ea1c5bf224
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2025-11-05 07:58:50 +0900

    refactor: staff-members APIで直接配列を返すように変更
    - jsonResponseListを使用してレスポンスを直接配列で返すように変更
    - StaffMemberListResponseのラッパーを削除
    - 不要になったdb_typesのimportを削除

--- bff/engine/lib/routes/staff_member_api_service.dart
+++ bff/engine/lib/routes/staff_member_api_service.dart
@@ -1,4 +1,3 @@
-import 'package:db_types/db_types.dart';
 import 'package:engine/main.dart';
 import 'package:engine/provider/db_client_provider.dart';
 import 'package:engine/util/json_response.dart';
@@ -10,17 +9,14 @@ part 'staff_member_api_service.g.dart';
 class StaffMemberApiService {
   /// スタッフメンバー一覧を取得
   @Route.get('/staff-members')
-  Future<Response> _getStaffMembers(Request request) async => jsonResponse(
+  Future<Response> _getStaffMembers(Request request) async => jsonResponseList(
     () async {
       final database = await container.read(
         dbClientProvider.future,
       );
       final staffMembers = await database.staffMember
           .getStaffMembersWithSnsLinks();
-
-      return StaffMemberListResponse(
-        staffMembers: staffMembers,
-      ).toJson();
+      return staffMembers.map((e) => e.toJson()).toList();
     },
   );
 
